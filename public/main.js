5e25a343abd7bcd902ad9ec91bcc8a98b1376f90
m1ga <migamiga@web.de>
2021-10-19 13:17:56 +0200

    update deps, store topic, variable update time

--- public/main.js
+++ public/main.js
@@ -1,7 +1,16 @@
-var socket = io('http://127.0.0.1:3000');
-var template = document.getElementById('template').innerHTML;
+const socket = io('http://127.0.0.1:3000');
+const template = document.getElementById('template').innerHTML;
 var autoUpdateTimer = null;
 var isAutoUpdate = false
+var topic = window.localStorage.getItem("twitchTopic");
+var updateTime = window.localStorage.getItem("updateTime") || 10000;
+
+document.getElementById("timeValue").value = Math.floor(updateTime / 1000);
+document.getElementById("autoUpdate").checked = false;
+
+if (topic != undefined) {
+	document.getElementById("topic").value = topic;
+}
 
 socket.on('data', function(obj) {
 	var output = "";
@@ -21,8 +30,13 @@ socket.on('data', function(obj) {
 	document.getElementById("loader").classList.remove("visible");
 })
 
+
+
 function update() {
 	if (!isAutoUpdate) document.getElementById("loader").classList.add("visible");
+
+	window.localStorage.setItem("twitchTopic", document.getElementById("topic").value);
+
 	socket.emit("update", {
 		topic: document.getElementById("topic").value,
 		lang: document.getElementById("language").value,
@@ -54,11 +68,30 @@ update();
 
 function autoUpdate() {
 	isAutoUpdate = document.getElementById("autoUpdate").checked;
+
+	if (updateTime < 5000) {
+		document.getElementById("timeValue").value = 5;
+		updateTime = 5000;
+	}
+
+	window.localStorage.setItem("updateTime", updateTime)
+
 	if (isAutoUpdate) {
 		autoUpdateTimer = setInterval(function() {
 			update();
-		}, 10000);
+		}, updateTime);
 	} else {
 		clearInterval(autoUpdateTimer);
+	}
+}
+
+function onChange() {
+	clearInterval(autoUpdateTimer);
+	document.getElementById("autoUpdate").checked = false;
+
+	var val = document.getElementById("timeValue").value;
+	val = parseInt(val);
+	if (Number.isInteger(val)) {
+		updateTime = val * 1000;
 	}
 }
